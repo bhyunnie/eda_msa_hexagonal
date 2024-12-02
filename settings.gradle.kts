d78e7b4be4f477c1fd37a82013be81fcdbbc2522
bhyunnie <dev.bhyunnie@gmail.com>
2024-12-02 09:59:44 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "eda_msa_hexagonal"
---
+++ src/main/kotlin/study/bhyunnie/eda_msa_hexagonal/EdaMsaHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package study.bhyunnie.eda_msa_hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdaMsaHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdaMsaHexagonalApplication>(*args)
+}
