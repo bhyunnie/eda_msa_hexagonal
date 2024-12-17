6587c5d7c58875956fba1f9982162cb40d993750
bhyunnie <dev.bhyunnie@gmail.com>
2024-12-17 23:23:14 +0900

    feat: Best Book 서비스 추가

---
+++ BestBookMS/src/main/kotlin/study/bhyunnie/bestbookms/BestBookMsApplication.kt
@@ -0,0 +1,11 @@
+package study.bhyunnie.bestbookms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BestBookMsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BestBookMsApplication>(*args)
+}
---
+++ BestBookMS/src/test/kotlin/study/bhyunnie/bestbookms/BestBookMsApplicationTests.kt
@@ -0,0 +1,12 @@
+package study.bhyunnie.bestbookms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BestBookMsApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+
+}
