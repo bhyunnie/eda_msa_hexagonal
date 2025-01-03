f4c8dd2d2075ca91ee6c3a3f7a6ebed5d4f78a21
bhyunnie <dev.bhyunnie@gmail.com>
2025-01-03 19:24:29 +0900

    feat: 루트프로젝트 gradle 추가

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.jpa") version "1.9.25"
+}
+
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
